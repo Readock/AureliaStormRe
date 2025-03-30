add49580cc24bdaec553d9d7fd6acf5f171c60e3
fhe <fheitbrock@invitec.com>
2025-03-30 15:06:34 +0200

    implement require folder suggestions

--- src/main/kotlin/com/github/denofevil/aurelia/require/AureliaRequireReferenceProvider.kt
+++ src/main/kotlin/com/github/denofevil/aurelia/require/AureliaRequireReferenceProvider.kt
@@ -1,5 +1,7 @@
 package com.github.denofevil.aurelia.require
 
+import com.intellij.lang.ecmascript6.resolve.JSFileReferencesUtil
+import com.intellij.psi.ElementManipulators
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
 import com.intellij.psi.PsiReferenceProvider
@@ -8,7 +10,13 @@ import com.intellij.psi.xml.XmlAttributeValue
 class AureliaRequireReferenceProvider : PsiReferenceProvider() {
     override fun getReferencesByElement(element: PsiElement, context: com.intellij.util.ProcessingContext): Array<PsiReference> {
         if (element is XmlAttributeValue) {
-            return arrayOf(AureliaRequireReference(element))
+            val text = ElementManipulators.getValueText(element)
+            val range = ElementManipulators.getValueTextRange(element)
+            val reference = AureliaRequireReference(element)
+            if (reference.resolve() != null) {
+                return arrayOf(reference)
+            }
+            return JSFileReferencesUtil.createImportExportFromClauseReferences(element, range.startOffset, text, this)
         }
         return PsiReference.EMPTY_ARRAY
     }
