17d1690fbbb61f20728f9e0569635a80bc7d9823
Collin Herber <rustaddicts@gmail.com>
2025-04-09 14:30:16 -0400

    perf: update json psi to use PackageJsonData

--- src/main/kotlin/com/github/denofevil/aurelia/Aurelia.kt
+++ src/main/kotlin/com/github/denofevil/aurelia/Aurelia.kt
@@ -1,20 +1,11 @@
 package com.github.denofevil.aurelia
 
-import com.intellij.json.psi.JsonFile
+import com.intellij.javascript.nodejs.PackageJsonData
+import com.intellij.javascript.nodejs.packageJson.PackageJsonFileManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ProjectRootModificationTracker
 import com.intellij.openapi.util.IconLoader
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.psi.util.CachedValueProvider
-import com.intellij.psi.util.CachedValuesManager
-import com.intellij.openapi.vfs.VfsUtilCore
-import com.intellij.json.psi.JsonObject
-import com.intellij.openapi.project.guessProjectDir
-import com.intellij.psi.PsiManager
+import com.tylerthrailkill.helpers.prettyprint.get
 
-/**
- * @author Dennis.Ushakov
- */
 object Aurelia {
     val ICON = IconLoader.getIcon("/icons/aurelia-icon.svg", Aurelia::class.java)
 
@@ -29,43 +20,19 @@ object Aurelia {
     const val THEN = "then"
     val CUSTOM_ELEMENTS = arrayOf("let")
 
-    fun present(project: Project) = CachedValuesManager.getManager(project).getCachedValue(project) {
-        val aureliaLoaded = hasDependency(project)
-        CachedValueProvider.Result
-            .create(aureliaLoaded, VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS, ProjectRootModificationTracker.getInstance(project))
-    }!!
+    fun present(project: Project): Boolean {
+        return hasDependency(project)
+    }
 
     private fun hasDependency(project: Project): Boolean {
-        var hasDependency = false
+        val manager = PackageJsonFileManager.getInstance(project)
 
-        project.guessProjectDir()?.let {
-            VfsUtilCore.iterateChildrenRecursively(it, null) { virtualFile ->
-                if (!virtualFile.isDirectory && virtualFile.name == "package.json") {
-                    val jsonFile = PsiManager.getInstance(project).findFile(virtualFile)
-                    val jsonObject = (jsonFile as? JsonFile)?.topLevelValue as? JsonObject
+        return manager.validPackageJsonFiles.any { virtualFile ->
+            val data = PackageJsonData.getOrCreate(virtualFile)
+            val dependencies = data.allDependencies;
 
-                    val dependencies = jsonObject?.findProperty("dependencies")?.value as? JsonObject
-                    val devDependencies = jsonObject?.findProperty("devDependencies")?.value as? JsonObject
-
-                    if (hasAureliaDependency(dependencies) || hasAureliaDependency(devDependencies)) {
-                        hasDependency = true
-                        return@iterateChildrenRecursively false
-                    }
-                }
-
-                true
-            }
+            dependencies["aurelia"] != null || dependencies["aurelia-cli"] != null
         }
-
-        return hasDependency
     }
 
-    private fun hasAureliaDependency(jsonObject: JsonObject?): Boolean {
-        if (jsonObject == null) return false
-
-        val aureliaDependency = jsonObject.findProperty("aurelia")
-        val aureliaCliDependency = jsonObject.findProperty("aurelia-cli")
-
-        return aureliaDependency != null || aureliaCliDependency != null
-    }
 }
