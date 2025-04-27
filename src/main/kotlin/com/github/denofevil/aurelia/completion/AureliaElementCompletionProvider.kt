3de598c42e3a6f4f00514ee995e5860357de187b
fhe <fheitbrock@invitec.com>
2025-04-27 11:50:14 +0200

    fix element suggestion showing in wrong places and insert complete tag text

--- src/main/kotlin/com/github/denofevil/aurelia/completion/AureliaElementCompletionProvider.kt
+++ src/main/kotlin/com/github/denofevil/aurelia/completion/AureliaElementCompletionProvider.kt
@@ -6,7 +6,7 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.psi.xml.XmlElement
+import com.intellij.psi.xml.XmlText
 import com.intellij.util.ProcessingContext
 
 class AureliaElementCompletionProvider : CompletionProvider<CompletionParameters>() {
@@ -15,9 +15,14 @@ class AureliaElementCompletionProvider : CompletionProvider<CompletionParameters
         context: ProcessingContext,
         result: CompletionResultSet
     ) {
-        val element = parameters.position.parent as? XmlElement ?: return
+        val element = parameters.position.parent as? XmlText ?: return
         AureliaIndexUtil.getAllCustomElementNames(element.project).forEach {
-            result.addElement(LookupElementBuilder.create(it).withIcon(Aurelia.CLASS_ICON))
+            result.addElement(
+                LookupElementBuilder.create(it).withIcon(Aurelia.CLASS_ICON)
+                    .withInsertHandler { context, _ ->
+                        context.editor.document.replaceString(context.startOffset, context.tailOffset, "<$it></$it>")
+                        context.editor.caretModel.moveToOffset(context.startOffset + it.length + 1)
+                    })
         }
     }
 }
